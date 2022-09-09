b89381e468aa84b8bfd6f48e64c661cc7aa1dbbe
api <110469682+api-was-taken@users.noreply.github.com>
2022-09-09 12:10:51 -0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+# ------------------------------------------------------- # Create Instance
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='>', intents=intents)
+# ------------------------------------------------------- # Configuration
+Logging = True # True or False
+token = 'input bot token'
+def staff(ctx):
+    return str(ctx.author.id) in settings["staffId"]
+# ------------------------------------------------------- # Commands
+@bot.slash_command(description="Pong!")
+async def ping(ctx):
+  if not staff(ctx): # If Not Staff, Will return Error
+    await ctx.respond(f"Error!", delete_after=3.0)
+  else:
+    await ctx.respond(f"Pong!")
+    if Logging == True: # If Logging = True, It will log commmand
+      await bot.get_channel(logging channel id).send(f"Ping Command has Been Run By: {ctx.author.mention}")
+    else:
+      pass # If Logging = False, Bot Will End Command
+    
+bot.run(token)
